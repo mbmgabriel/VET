db5e803ddcb329ae71a4459386e2cf5a59d2869a
Laurence Bautista <rorens05@gmail.com>
2022-05-30 10:18:55 +0800

    Integrate feeds

--- src/views/calendar/CalendarPage.js
+++ src/views/calendar/CalendarPage.js
@@ -1,25 +1,44 @@
 import moment from "moment";
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Calendar, Views, momentLocalizer } from "react-big-calendar";
+import { toast } from "react-toastify";
+import FeedsAPI from "../../api/FeedsAPI";
 import MainContainer from "../../components/layouts/MainContainer";
+import { UserContext } from "../../context/UserContext";
 const localizer = momentLocalizer(moment); // or globalizeLocalizer
-let allViews = Object.keys(Views).map((k) => Views[k]);
 
-const EVENTS = [
-  {
-    start: moment().toDate(),
-    end: moment().add(1, "days").toDate(),
-    title: `Some title  ${moment().format('LT')}`,
-  },
-  {
-    start: moment().subtract(1, "hours").toDate(),
-    end: moment().toDate(),
-    title: `Test ${moment().subtract(1, "hours").format('LT')}`,
-  },
-];
 
 export default function CalendarPage() {
   const [loading, setLoading] = useState(false);
+  const [events, setEvents] = useState([]);
+  const userContext = useContext(UserContext)
+  const {user} = userContext.data
+  
+  const fetchEvents = async() => {
+    setLoading(true);
+    const response = await new FeedsAPI().fetchEvents(user?.userId);
+    if(response.ok){
+      let tempEvents = response.data.map(item => {
+        let start = new Date(item.startDate)
+        let end = new Date(item.endDate)
+
+        return {
+          start,
+          end,
+          title: item.title,
+        };
+      })
+      console.log({tempEvents})
+      setEvents(tempEvents);
+    }else{
+      toast.error("Something went wrong while fetching events")
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchEvents()
+  }, [])
 
   return (
     <MainContainer activeHeader={"classes"} loading={loading}>
@@ -29,11 +48,11 @@ export default function CalendarPage() {
             <Calendar
               localizer={localizer}
               dayLayoutAlgorithm='no-overlap'
-              events={EVENTS}
+              events={events}
               startAccessor='start'
               endAccessor='end'
               defaultView='month'
-              onSelectEvent={(data) => alert(`You click ${data.title}`)}
+              onSelectEvent={(data) => console.log(`You click ${data.title}`)}
 
             />
           </div>
