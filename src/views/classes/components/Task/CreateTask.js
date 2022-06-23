8b20daada25dae1bdf4222c27301d0f42e4fda33
ferrerUnk <unkferrer@gmail.com>
2022-06-23 16:29:36 +0800

    Fix displaying of files on task creation

--- src/views/classes/components/Task/CreateTask.js
+++ src/views/classes/components/Task/CreateTask.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Modal from 'react-bootstrap/Modal'
-import { Form, Button, } from 'react-bootstrap'
+import { Form, Button, Table, Tooltip, OverlayTrigger} from 'react-bootstrap'
 import ClassesAPI from '../../../../api/ClassesAPI'
 import FilesAPI from '../../../../api/FilesApi';
 import FileHeader from './TaskFileHeader';
@@ -17,6 +17,11 @@ function CreateTask({setModal, modal, toggle, module, getTaskModule, classId}) {
   const [displayFiles, setDisplayFiles] = useState([]);
   const [showFiles, setShowFiles] = useState(false);
   const [displayFolder, setDisplayFolder] = useState([]);
+  const [breedCrumbsItemClass, setBreedCrumbsItemClass] = useState([])
+  const subFolderDirectory = breedCrumbsItemClass.map(item => { return `/${item.value}`}) //to get sub directory based on breedcrumbs
+  const [displayType, setDisplayType] = useState('');
+  const [showFilesFolders, setShowFilesFolders] = useState(false);
+  const [courseId, setCourseId] = useState(null)
   const allowLate = true
   const {id} = useParams();
 
@@ -32,12 +37,26 @@ function CreateTask({setModal, modal, toggle, module, getTaskModule, classId}) {
   }
 
   useEffect(() => {
-    handleGetClassFiles()
+    getClassInfo()
     console.log(module, '-----------')
   }, [])
-  const handleGetClassFiles = async() => {
+
+  const getClassInfo = async() => {
     // setLoading(true)
-    let response = await new FilesAPI().getClassFiles(classId)
+    let response = await new ClassesAPI().getClassInformation(id)
+    if(response.ok){
+      setCourseId(response?.data?.courseId)
+      console.log({response})
+    }
+    // setLoading(false)
+  }
+
+  const handleGetClassFiles = async(name) => {
+    // setLoading(true)
+    let data = {
+      "subFolderLocation": name
+    }
+    let response = await new FilesAPI().getClassFiles(classId, data)
     // setLoading(false)
     if(response.ok){
       setDisplayFiles(response.data.files)
@@ -45,7 +64,23 @@ function CreateTask({setModal, modal, toggle, module, getTaskModule, classId}) {
     }else{
       alert("Something went wrong while fetching class files ;;.")
     }
-  } 
+  }
+
+  const handleGetCourseFiles = async(name) => {
+    // setLoading(true)
+    let data = {
+      "subFolderLocation": name
+    }
+    let response = await new FilesAPI().getCourseFiles(courseId, data)
+    // setLoading(false)
+    if(response.ok){
+      setDisplayFiles(response.data.files)
+      setDisplayFolder(response.data.folders)
+    }else{
+      alert("Something went wrong while fetching Course files.")
+    }
+  }
+
   
 
   const success = () => {
@@ -96,6 +131,76 @@ function CreateTask({setModal, modal, toggle, module, getTaskModule, classId}) {
     }
   }
 
+  const handleClickedClassBread = () => {
+    handleGetClassFiles('')
+    setBreedCrumbsItemClass([])
+  }
+
+  const handleClickedBreadcrumbsItem = (value, index, type) => {
+    if(type == 'Class'){
+      subFolderDirectory.length = index+1;
+      breedCrumbsItemClass.length = index+1;
+      handleGetClassFiles(subFolderDirectory.join(''));
+    }
+  }
+
+  const handleClickedItem = (data) => {
+    setDisplayType(data);
+    // setDisplayClassCourse(true);
+    if(data == 'Class'){
+      handleGetClassFiles('');
+    }else{
+      handleGetCourseFiles('');
+    }
+  }
+
+  const handleClickedFolder = (name, type) =>{
+    if(type == 'Class'){
+      let temp = {
+        naame: name,
+        value: name
+      }
+      breedCrumbsItemClass.push(temp)
+      setBreedCrumbsItemClass(breedCrumbsItemClass);
+      handleGetClassFiles(`${subFolderDirectory.join('')}/${name}`);
+    }else{
+      let temp = {
+        naame: name,
+        value: name
+      }
+      breedCrumbsItemClass.push(temp)
+      setBreedCrumbsItemClass(breedCrumbsItemClass);
+      handleGetCourseFiles(`${subFolderDirectory.join('')}/${name}`);
+    }
+  }
+
+  const handleClickType = (type) => {
+    setDisplayType(type);
+    setShowFilesFolders(!showFilesFolders)
+    setBreedCrumbsItemClass([]);
+    if(type == 'Class'){
+      handleGetClassFiles('')
+    }
+    if(type == 'Course'){
+      handleGetCourseFiles('')
+    }
+  }
+
+  const clickFile = (link) => {
+    navigator.clipboard.writeText(link)
+    toast.success('File link copied to clipboard.')
+  }
+
+  const handleFileBreed = () => {
+    setBreedCrumbsItemClass([]);
+    if(displayType == 'Class'){
+      handleGetClassFiles('')
+    }
+    if(displayType == 'Course'){
+      handleGetCourseFiles('')
+    }
+  }
+
 	return (
     <div>
     	<Modal size="lg" show={modal} onHide={handleCloseModal} aria-labelledby="example-modal-sizes-title-lg">
@@ -107,31 +212,79 @@ function CreateTask({setModal, modal, toggle, module, getTaskModule, classId}) {
         <Modal.Body>
         <Form onSubmit={saveTask} >  
           <div className={showFiles ? 'mb-3' : 'd-none'}>
-            <FileHeader type='Class' id={classId}  subFolder={''}  doneUpload={()=> handleGetClassFiles()} />
-            {/* {
-             (displayFiles || []).map( (item,ind) => {
-                return(
-                  <img src={item.pathBase.replace('http:', 'https:')} className='p-1' alt={item.fileName} height={30} width={30}/>
-                )
-              })
-            } */}
-             {
-               (displayFiles || []).map( (item,ind) => {
-                  return(
-                    item.pathBase?.match(/.(jpg|jpeg|png|gif|pdf)$/i) ? 
-                    <img key={ind+item.name} src={item.pathBase.replace('http:', 'https:')} className='p-1' alt={item.name} height={30} width={30}/>
-                    :
-                    <i className="fas fa-sticky-note" style={{paddingRight: 5}}/>
-                  )
-                })
-              }
+            {displayType == 'Class' ?
+              <FileHeader type='Class' id={classId}  subFolder={''}  doneUpload={()=> handleGetClassFiles()} />
+              :
+              <div>
+                <p className='title-header'>Files</p>
+              </div>
+            }
+            <div>
+              <span onClick={()=> handleClickType('')} className={displayType ? 'colored-class-task' : 'fix-color-bread'}>Files</span>
+              {displayType && <span onClick={()=> handleFileBreed()} className={breedCrumbsItemClass.length == 0 ? 'fix-color-bread' : 'colored-class-task'}> <i class="fas fa-chevron-right m-l-10 m-r-10"></i> {displayType} Files</span>}
               {
-                (displayFolder || []).map((itm) => {
-                  return(
-                    <i className='fas fa-folder-open' style={{height: 30, width: 30}}/>
-                  )
+                breedCrumbsItemClass.map((item, index) => {
+                  return <span onClick={() => handleClickedBreadcrumbsItem(item.value, index, 'Class')} className={breedCrumbsItemClass.length == (index+1) ? 'fix-color-bread' : 'colored-class-task'}>  <i class="fas fa-chevron-right m-l-10 m-r-10"></i> {item.naame}</span>
                 })
               }
+            </div>
+            { showFilesFolders ?
+              <>
+              {
+                displayFiles.map( (item,ind) => {
+                  console.log(item)
+                    return(
+                      <OverlayTrigger
+                        placement="bottom"
+                        delay={{ show: 1, hide: 0 }}
+                        overlay={(props) => 
+                          <Tooltip id="button-tooltip" {...props}>
+                            {item.name}
+                          </Tooltip>}
+                      >
+                     {item.pathBase?.match(/.(jpg|jpeg|png|gif|pdf)$/i) ? 
+                        <img key={ind+item.name} src={item.pathBase.replace('http:', 'https:')} onClick={() => clickFile(item.pathBase)} className='p-1' alt={item.name} height={30} width={30}/>
+                        :
+                        <i className="fas fa-sticky-note" onClick={() => clickFile(item.pathBase)} style={{paddingRight: 5}}/>
+                      }
+                      </OverlayTrigger>
+                    )
+                  })
+                }
+                {
+                  displayFolder.map((itm) => {
+                    return(
+                      <OverlayTrigger
+                        placement="bottom"
+                        delay={{ show: 1, hide: 0 }}
+                        overlay={(props) => 
+                          <Tooltip id="button-tooltip" {...props}>
+                            {itm.name}
+                          </Tooltip>}
+                      >
+                        <i className='fas fa-folder-open' onClick={()=> handleClickedFolder(itm.name, displayType)} title='' style={{height: 30, width: 30}}/>
+                      </OverlayTrigger>
+                    )
+                  })
+                }
+              </>
+              :
+              <Table responsive="sm">
+                <thead>
+                  <tr>
+                    <th>Name</th>  {/* icon for sorting <i class="fas fa-sort-alpha-down td-file-page"></i> */}
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td colSpan={3} onClick={() => handleClickType('Class')} className='ellipsis w-25 task-folder'><i className="fas fa-folder" /><span> Class Files</span></td>
+                  </tr>
+                  <tr>
+                    <td colSpan={3} onClick={() => handleClickType('Course')} className='ellipsis w-25 task-folder'><i className="fas fa-folder" /><span> Course Files</span></td>
+                  </tr>
+                </tbody>
+              </Table>
+            }
           </div>
           <Form.Group className="mb-3">
           <Form.Label>Unit</Form.Label>
