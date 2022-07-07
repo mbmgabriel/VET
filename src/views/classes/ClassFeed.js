28474b57db00aae26d9db28b4a835ae4175cd39a
Krent9263 <placia.kent@gmail.com>
2022-07-07 20:27:31 +0800

    add froala in feed and discussion

--- src/views/classes/ClassFeed.js
+++ src/views/classes/ClassFeed.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react'
-import {Card, InputGroup, FormControl, Row, Col,Button, Form, Tooltip, OverlayTrigger, Fade} from 'react-bootstrap'
+import {Card, InputGroup, FormControl, Row, Col,Button, Form, Tooltip, OverlayTrigger, Fade, Table} from 'react-bootstrap'
 import ClassesAPI from '../../api/ClassesAPI'
 import SweetAlert from 'react-bootstrap-sweetalert';
 import EditAnnouncement from './components/Feed/EditAnnouncement';
@@ -13,6 +13,10 @@ import AnnouncementComment from './components/Feed/AnnouncementComment';
 import DiscussionAPI from '../../api/DiscussionAPI';
 import {Link} from 'react-router-dom'
 import ShowLike from './components/Feed/ShowLike';
+import ContentField from '../../components/content_field/ContentField';
+import FilesAPI from '../../api/FilesApi';
+import FileHeader from '../classes/components/Task/TaskFileHeader';
+import ContentViewer from '../../components/content_field/ContentViewer';
 
 function ClassFeed() {
   const [content, setContent] = useState('')
@@ -37,6 +41,16 @@ function ClassFeed() {
   const [showLike, setShowLike] = useState(false)
   const [feedItemLike, setFeedItemLike] = useState([])
   const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+
+  const [displayFiles, setDisplayFiles] = useState([]);
+  const [showFilesFolders, setShowFilesFolders] = useState(false);
+  const [breedCrumbsItemClass, setBreedCrumbsItemClass] = useState([])
+  const [showFiles, setShowFiles] = useState(false);
+  const [displayType, setDisplayType] = useState('');
+  const subFolderDirectory = breedCrumbsItemClass.map(item => { return `/${item.value}`}) //to get sub directory based on breedcrumbs
+  const [displayFolder, setDisplayFolder] = useState([]);
+  const [courseId, setCourseId] = useState(null)
+  const classId = id
 
   const closeNotify = () =>{
     setAddNotity(false)
@@ -80,7 +94,6 @@ function ClassFeed() {
   }
 
   const getClassInfo = async() => {
-  
     let response = await new DiscussionAPI().getClassInfo(id)
     if(response.ok){
       setClassInfo(response.data)
@@ -89,6 +102,20 @@ function ClassFeed() {
     }
   
   }
+
+  const getClassInformation = async() => {
+    // setLoading(true)
+    let response = await new ClassesAPI().getClassInformation(id)
+    if(response.ok){
+      setCourseId(response?.data?.courseId)
+      console.log({response})
+    }
+    // setLoading(false)
+  }
+
+  useEffect(() => {
+    getClassInformation();
+  }, [])
 
   useEffect(() => {
     getClassInfo();
@@ -178,6 +205,92 @@ const getComment = (item, item1, item3) => {
       }
   }
 
+  const clickFile = (link) => {
+    navigator.clipboard.writeText(link)
+    toast.success('File link copied to clipboard.')
+  }
+
+  const handleClickedBreadcrumbsItem = (value, index, type) => {
+    if(type == 'Class'){
+      subFolderDirectory.length = index+1;
+      breedCrumbsItemClass.length = index+1;
+      handleGetClassFiles(subFolderDirectory.join(''));
+    }
+  }
+
+  const handleClickType = (type) => {
+    setDisplayType(type);
+    setShowFilesFolders(!showFilesFolders)
+    setBreedCrumbsItemClass([]);
+    if(type == 'Class'){
+      handleGetClassFiles('')
+    }
+    if(type == 'Course'){
+      handleGetCourseFiles('')
+    }
+  }
+
+  const handleGetClassFiles = async(name) => {
+    // setLoading(true)
+    let data = {
+      "subFolderLocation": name
+    }
+    let response = await new FilesAPI().getClassFiles(classId, data)
+    // setLoading(false)
+    if(response.ok){
+      setDisplayFiles(response.data.files)
+      setDisplayFolder(response.data.folders)
+    }else{
+      alert("Something went wrong while fetching class files ;;.")
+    }
+  }
+
+  const handleGetCourseFiles = async(name) => {
+    // setLoading(true)
+    let data = {
+      "subFolderLocation": name
+    }
+    let response = await new FilesAPI().getCourseFiles(courseId, data)
+    // setLoading(false)
+    if(response.ok){
+      setDisplayFiles(response.data.files)
+      setDisplayFolder(response.data.folders)
+    }else{
+      alert(courseId)
+      alert("Something went wrong while fetching Course files.111111111111111")
+    }
+  }
+
+  const handleFileBreed = () => {
+    setBreedCrumbsItemClass([]);
+    if(displayType == 'Class'){
+      handleGetClassFiles('')
+    }
+    if(displayType == 'Course'){
+      handleGetCourseFiles('')
+    }
+  }
+
+  const handleClickedFolder = (name, type) =>{
+    if(type == 'Class'){
+      let temp = {
+        naame: name,
+        value: name
+      }
+      breedCrumbsItemClass.push(temp)
+      setBreedCrumbsItemClass(breedCrumbsItemClass);
+      handleGetClassFiles(`${subFolderDirectory.join('')}/${name}`);
+    }else{
+      let temp = {
+        naame: name,
+        value: name
+      }
+      breedCrumbsItemClass.push(temp)
+      setBreedCrumbsItemClass(breedCrumbsItemClass);
+      handleGetCourseFiles(`${subFolderDirectory.join('')}/${name}`);
+    }
+  }
+
   const renderTooltipLike= (props) => (
     <Tooltip id="button-tooltip" {...props}>
       Like
@@ -225,11 +338,92 @@ const getComment = (item, item1, item3) => {
           <Card className='calendar-card'>
             <Card.Body>
             <Form onSubmit={createAnnouncementClass}>
-              <InputGroup  size="lg">
+            <div className={showFiles ? 'mb-3' : 'd-none'}>
+            {displayType == 'Class' ?
+              <FileHeader type='Class' id={classId}  subFolder={`${subFolderDirectory.join('')}`}  doneUpload={()=> handleGetClassFiles(subFolderDirectory.join(''))} />
+              :
+              <div>
+                <p className='title-header'>Files</p>
+              </div>
+            }
+            <div>
+              <span onClick={()=> handleClickType('')} className={displayType ? 'colored-class-task' : 'fix-color-bread'}>Files</span>
+              {displayType && <span onClick={()=> handleFileBreed()} className={breedCrumbsItemClass.length == 0 ? 'fix-color-bread' : 'colored-class-task'}> <i class="fas fa-chevron-right m-l-10 m-r-10"></i> {displayType} Files</span>}
+              {
+                breedCrumbsItemClass.map((item, index) => {
+                  return <span onClick={() => handleClickedBreadcrumbsItem(item.value, index, 'Class')} className={breedCrumbsItemClass.length == (index+1) ? 'fix-color-bread' : 'colored-class-task'}>  <i class="fas fa-chevron-right m-l-10 m-r-10"></i> {item.naame}</span>
+                })
+              }
+            </div>
+            { showFilesFolders ?
+              <>
+              {
+                displayFiles.map( (item,ind) => {
+                  console.log(item)
+                    return(
+                      <OverlayTrigger
+                        placement="bottom"
+                        delay={{ show: 1, hide: 0 }}
+                        overlay={(props) => 
+                          <Tooltip id="button-tooltip" {...props}>
+                            {item.name}
+                          </Tooltip>}
+                      >
+                     {item.pathBase?.match(/.(jpg|jpeg|png|gif|pdf)$/i) ? 
+                        <img key={ind+item.name} src={item.pathBase.replace('http:', 'https:')} onClick={() => clickFile(item.pathBase)} className='p-1' alt={item.name} height={30} width={30}/>
+                        :
+                        <i className="fas fa-sticky-note" onClick={() => clickFile(item.pathBase)} style={{paddingRight: 5}}/>
+                      }
+                      </OverlayTrigger>
+                    )
+                  })
+                }
+                {
+                  displayFolder.map((itm) => {
+                    return(
+                      <OverlayTrigger
+                        placement="bottom"
+                        delay={{ show: 1, hide: 0 }}
+                        overlay={(props) => 
+                          <Tooltip id="button-tooltip" {...props}>
+                            {itm.name}
+                          </Tooltip>}
+                      >
+                        <i className='fas fa-folder-open' onClick={()=> handleClickedFolder(itm.name, displayType)} title='' style={{height: 30, width: 30}}/>
+                      </OverlayTrigger>
+                    )
+                  })
+                }
+              </>
+              :
+              <Table responsive="sm">
+                <thead>
+                  <tr>
+                    <th>Name</th>  {/* icon for sorting <i class="fas fa-sort-alpha-down td-file-page"></i> */}
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td colSpan={3} onClick={() => handleClickType('Class')} className='ellipsis w-25 task-folder'><i className="fas fa-folder" /><span> Class Files</span></td>
+                  </tr>
+                  <tr>
+                    <td colSpan={3} onClick={() => handleClickType('Course')} className='ellipsis w-25 task-folder'><i className="fas fa-folder" /><span> Course Files</span></td>
+                  </tr>
+                </tbody>
+              </Table>
+            }
+          </div> 
+              {/* <InputGroup  size="lg">
                 <InputGroup.Text id="basic-addon2" className="feed-button"><i class="fas fa-user-circle fas-1x" ></i></InputGroup.Text>
                   <FormControl onChange={(e) => setContent(e.target.value)} value={content} className='feed-box'  aria-label="small" aria-describedby="inputGroup-sizing-sm" placeholder="Type an Announcement for the class here" type="text"/> 
-              </InputGroup>
+              </InputGroup> */}
+                {/* <Form.Group  style={{display:'inline-flex'}} >
+                  <Form.Label className="feed-button" ><i class="fas fa-user-circle fas-1x" ></i></Form.Label>
+                    <ContentField value={content}  placeholder='Enter instruction here'  onChange={value => setContent(value)} />
+                  </Form.Group> */}
+                  <ContentField value={content}  placeholder='Enter Announcement here'  onChange={value => setContent(value)} />
               <div style={{textAlign:'right', paddingTop:'15px'}}>
+              <Button onClick={()=> setShowFiles(!showFiles)}>File Library</Button>&nbsp;
               <Button disabled={isButtonDisabled} className='tficolorbg-button' type='submit' >POST</Button>
               </div>
             </Form>
@@ -302,7 +496,9 @@ const getComment = (item, item1, item3) => {
                   {/* <i class="fas fa-file-alt" style={{color:'#EE9337', fontSize:'30px',}}></i> */}
                 </Col>
                 <Col sm={11} style={{fontSize:'16px', color:'#707070', textAlign: 'justify'}}>
-                 <p className='font-color'>{feedItem.description}</p>
+                 <div className='font-color' >
+                   <ContentViewer>{feedItem.description}</ContentViewer>
+                 </div>
                 </Col>
               </Row>
               <Row>
--- src/views/classes/components/Discussion/CreateDiscussion.js
+++ src/views/classes/components/Discussion/CreateDiscussion.js
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import Modal from 'react-bootstrap/Modal'
-import { Form, Button, } from 'react-bootstrap'
+import { Form, Button, Tooltip, OverlayTrigger, Table} from 'react-bootstrap'
 import ClassesAPI from '../../../../api/ClassesAPI'
 import { useParams } from 'react-router'
 import SweetAlert from 'react-bootstrap-sweetalert';
 import { toast } from 'react-toastify';
+import FilesAPI from '../../../../api/FilesApi';
+import FileHeader from './../Task/TaskFileHeader';
+import ContentField from '../../../../components/content_field/ContentField'
 
 function CreateDiscussion({setModal, modal, toggle, classInfo, module, getDiscussionUnit}) {
   const [moduleId, setModuleId] = useState('')
   const [discussionName, setDiscussionName] = useState('')
   const [instructions, setInstructions] = useState('')
   const [addNotify, setAddNotity] = useState(false)
-  const courseId = classInfo?.classInformation?.courseId
+  const [displayFiles, setDisplayFiles] = useState([]);
+  const [showFilesFolders, setShowFilesFolders] = useState(false);
+  const [breedCrumbsItemClass, setBreedCrumbsItemClass] = useState([])
+  const [showFiles, setShowFiles] = useState(false);
+  const [displayType, setDisplayType] = useState('');
+  const subFolderDirectory = breedCrumbsItemClass.map(item => { return `/${item.value}`}) //to get sub directory based on breedcrumbs
+  const [displayFolder, setDisplayFolder] = useState([]);
+  const [courseId, setCourseId] = useState(null)
   const allowLate = true
   const {id} = useParams();
+  const classId = id
 
   const closeNotify = () =>{
     setAddNotity(false)
@@ -24,6 +35,21 @@ function CreateDiscussion({setModal, modal, toggle, classInfo, module, getDiscus
     setInstructions('')
     setModuleId('')
     setModal(false)
+  }
+
+  useEffect(() => {
+    getClassInfo()
+    console.log(module, '-----------')
+  }, [])
+
+  const getClassInfo = async() => {
+    // setLoading(true)
+    let response = await new ClassesAPI().getClassInformation(id)
+    if(response.ok){
+      setCourseId(response?.data?.courseId)
+      console.log({response})
+    }
+    // setLoading(false)
   }
 
   const saveDiscussion = async (e) =>{
@@ -73,6 +99,92 @@ function CreateDiscussion({setModal, modal, toggle, classInfo, module, getDiscus
     });
   }
 
+  const clickFile = (link) => {
+    navigator.clipboard.writeText(link)
+    toast.success('File link copied to clipboard.')
+  }
+
+  const handleClickedBreadcrumbsItem = (value, index, type) => {
+    if(type == 'Class'){
+      subFolderDirectory.length = index+1;
+      breedCrumbsItemClass.length = index+1;
+      handleGetClassFiles(subFolderDirectory.join(''));
+    }
+  }
+
+  const handleClickType = (type) => {
+    setDisplayType(type);
+    setShowFilesFolders(!showFilesFolders)
+    setBreedCrumbsItemClass([]);
+    if(type == 'Class'){
+      handleGetClassFiles('')
+    }
+    if(type == 'Course'){
+      handleGetCourseFiles('')
+    }
+  }
+
+  const handleGetClassFiles = async(name) => {
+    // setLoading(true)
+    let data = {
+      "subFolderLocation": name
+    }
+    let response = await new FilesAPI().getClassFiles(classId, data)
+    // setLoading(false)
+    if(response.ok){
+      setDisplayFiles(response.data.files)
+      setDisplayFolder(response.data.folders)
+    }else{
+      alert("Something went wrong while fetching class files ;;.")
+    }
+  }
+
+  const handleGetCourseFiles = async(name) => {
+    // setLoading(true)
+    let data = {
+      "subFolderLocation": name
+    }
+    let response = await new FilesAPI().getCourseFiles(courseId, data)
+    // setLoading(false)
+    if(response.ok){
+      setDisplayFiles(response.data.files)
+      setDisplayFolder(response.data.folders)
+    }else{
+      alert(courseId)
+      alert("Something went wrong while fetching Course files.111111111111111")
+    }
+  }
+
+  const handleFileBreed = () => {
+    setBreedCrumbsItemClass([]);
+    if(displayType == 'Class'){
+      handleGetClassFiles('')
+    }
+    if(displayType == 'Course'){
+      handleGetCourseFiles('')
+    }
+  }
+
+  const handleClickedFolder = (name, type) =>{
+    if(type == 'Class'){
+      let temp = {
+        naame: name,
+        value: name
+      }
+      breedCrumbsItemClass.push(temp)
+      setBreedCrumbsItemClass(breedCrumbsItemClass);
+      handleGetClassFiles(`${subFolderDirectory.join('')}/${name}`);
+    }else{
+      let temp = {
+        naame: name,
+        value: name
+      }
+      breedCrumbsItemClass.push(temp)
+      setBreedCrumbsItemClass(breedCrumbsItemClass);
+      handleGetCourseFiles(`${subFolderDirectory.join('')}/${name}`);
+    }
+  }
+
 	return (
     <div>
     	<Modal size="lg" show={modal} onHide={handleCloseModal} aria-labelledby="example-modal-sizes-title-lg">
@@ -82,7 +194,82 @@ function CreateDiscussion({setModal, modal, toggle, classInfo, module, getDiscus
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form onSubmit={saveDiscussion} >  
+        <Form onSubmit={saveDiscussion} >
+        <div className={showFiles ? 'mb-3' : 'd-none'}>
+            {displayType == 'Class' ?
+              <FileHeader type='Class' id={classId}  subFolder={`${subFolderDirectory.join('')}`}  doneUpload={()=> handleGetClassFiles(subFolderDirectory.join(''))} />
+              :
+              <div>
+                <p className='title-header'>Files</p>
+              </div>
+            }
+            <div>
+              <span onClick={()=> handleClickType('')} className={displayType ? 'colored-class-task' : 'fix-color-bread'}>Files</span>
+              {displayType && <span onClick={()=> handleFileBreed()} className={breedCrumbsItemClass.length == 0 ? 'fix-color-bread' : 'colored-class-task'}> <i class="fas fa-chevron-right m-l-10 m-r-10"></i> {displayType} Files</span>}
+              {
+                breedCrumbsItemClass.map((item, index) => {
+                  return <span onClick={() => handleClickedBreadcrumbsItem(item.value, index, 'Class')} className={breedCrumbsItemClass.length == (index+1) ? 'fix-color-bread' : 'colored-class-task'}>  <i class="fas fa-chevron-right m-l-10 m-r-10"></i> {item.naame}</span>
+                })
+              }
+            </div>
+            { showFilesFolders ?
+              <>
+              {
+                displayFiles.map( (item,ind) => {
+                  console.log(item)
+                    return(
+                      <OverlayTrigger
+                        placement="bottom"
+                        delay={{ show: 1, hide: 0 }}
+                        overlay={(props) => 
+                          <Tooltip id="button-tooltip" {...props}>
+                            {item.name}
+                          </Tooltip>}
+                      >
+                     {item.pathBase?.match(/.(jpg|jpeg|png|gif|pdf)$/i) ? 
+                        <img key={ind+item.name} src={item.pathBase.replace('http:', 'https:')} onClick={() => clickFile(item.pathBase)} className='p-1' alt={item.name} height={30} width={30}/>
+                        :
+                        <i className="fas fa-sticky-note" onClick={() => clickFile(item.pathBase)} style={{paddingRight: 5}}/>
+                      }
+                      </OverlayTrigger>
+                    )
+                  })
+                }
+                {
+                  displayFolder.map((itm) => {
+                    return(
+                      <OverlayTrigger
+                        placement="bottom"
+                        delay={{ show: 1, hide: 0 }}
+                        overlay={(props) => 
+                          <Tooltip id="button-tooltip" {...props}>
+                            {itm.name}
+                          </Tooltip>}
+                      >
+                        <i className='fas fa-folder-open' onClick={()=> handleClickedFolder(itm.name, displayType)} title='' style={{height: 30, width: 30}}/>
+                      </OverlayTrigger>
+                    )
+                  })
+                }
+              </>
+              :
+              <Table responsive="sm">
+                <thead>
+                  <tr>
+                    <th>Name</th>  {/* icon for sorting <i class="fas fa-sort-alpha-down td-file-page"></i> */}
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td colSpan={3} onClick={() => handleClickType('Class')} className='ellipsis w-25 task-folder'><i className="fas fa-folder" /><span> Class Files</span></td>
+                  </tr>
+                  <tr>
+                    <td colSpan={3} onClick={() => handleClickType('Course')} className='ellipsis w-25 task-folder'><i className="fas fa-folder" /><span> Course Files</span></td>
+                  </tr>
+                </tbody>
+              </Table>
+            }
+          </div>  
           <Form.Group className="mb-3">
           <Form.Label>Unit</Form.Label>
             <Form.Select onChange={(e) => setModuleId(e.target.value)}>
@@ -91,6 +278,9 @@ function CreateDiscussion({setModal, modal, toggle, classInfo, module, getDiscus
                   return(<option value={item.id}>{item.moduleName}</option>)
                 })}
             </Form.Select>
+            <div>
+              <Button className='float-right my-2 tficolorbg-button' onClick={()=> setShowFiles(!showFiles)}>File Library</Button>
+            </div>
               </Form.Group>
               <Form.Group className="mb-4">
                 <Form.Label>Discussion Name</Form.Label>
@@ -98,7 +288,7 @@ function CreateDiscussion({setModal, modal, toggle, classInfo, module, getDiscus
                 </Form.Group>
                 <Form.Group className="mb-4">
                   <Form.Label >Instruction</Form.Label>
-                    <Form.Control onChange={(e) => setInstructions(e.target.value)} type="text" placeholder='Enter instruction here'/>
+                    <ContentField value={instructions}  placeholder='Enter instruction here'  onChange={value => setInstructions(value)} />
                   </Form.Group>
               <Form.Group className='right-btn'>
               <Button className='tficolorbg-button' type='submit' >Save</Button>
