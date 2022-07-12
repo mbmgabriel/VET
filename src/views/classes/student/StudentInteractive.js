0ad7153edb4ad98f1e68e13130b87dd9041532c3
Michael Ben Gabriel <michaelbengabriel@Michaels-iMac.example.com>
2022-07-12 12:33:24 +0800

    gp student

--- src/views/classes/student/StudentInteractive.js
+++ src/views/classes/student/StudentInteractive.js
@@ -422,25 +422,12 @@ const renderTooltipPlay1 = (props) => (
                     <>
                     <Col sm={3} className='icon-exam'>
                         {
-                          item.module.courseId === 50005 ?
-                          <OverlayTrigger
-                          placement="right"
-                          delay={{ show: 10, hide: 25 }}
-                          overlay={renderTooltipPlay}>
-                          <button onClick={(e) => {
-                            goLinkGodsPark(item?.interactive?.path)
-                            }} className="btn btn-xs btn-green" >
-                                {item?.interactive?.interactiveName} Start
-                                {schoolCode.id}
-                          </button>
-                          </OverlayTrigger>
+                          classInfo?.classInformation?.courseName === "God's Park" || classInfo?.classInformation?.courseName === "God's Park 1" || classInfo?.classInformation?.courseName === "God's Park 2" ||
+                          classInfo?.classInformation?.courseName === "God's Park 3" || classInfo?.classInformation?.courseName === "God's Park 4" ||
+                          classInfo?.classInformation?.courseName === "God's Park 5" || classInfo?.classInformation?.courseName === "God's Park 6" ?
+                          <Button onClick={(e) => goLinkGodsPark(item?.interactive?.path)}  className="m-r-5 color-white tficolorbg-button" size="sm"><i class="fas fa-play" ></i></Button>
                           :
-                          <OverlayTrigger
-                          placement="right"
-                          delay={{ show: 10, hide: 25 }}
-                          overlay={renderTooltipPlay}> 
-                            <Button onClick={(e) => getInteractiveLink(e, item?.interactive?.path, user?.student?.id, item?.interactive?.id, id)}  className="m-r-5 color-white tficolorbg-button" size="sm"><i class="fas fa-play" ></i></Button>  
-                            </OverlayTrigger>
+                          <Button onClick={(e) => getInteractiveLink(e, item?.interactive?.path, user?.student?.id, item?.interactive?.id, id)}  className="m-r-5 color-white tficolorbg-button" size="sm"><i class="fas fa-play" ></i></Button>
                         }
                     </Col>
                     </>
@@ -476,13 +463,17 @@ const renderTooltipPlay1 = (props) => (
                         delay={{ show: 10, hide: 25 }}
                         overlay={renderTooltipPlay}> 
                           {
-                          item.module.courseId === 50005 ?
-                          <button onClick={(e) => {
-                            goLinkGodsPark(item?.interactive?.path)
-                            }} className="btn btn-xs btn-green" >
-                                {item?.interactive?.interactiveName} Start
-                                {schoolCode.id}
-                          </button>
+                          // item.module.courseId === 50005 ?
+                          // <button onClick={(e) => {
+                          //   goLinkGodsPark(item?.interactive?.path)
+                          //   }} className="btn btn-xs btn-green" >
+                          //       {item?.interactive?.interactiveName} Start
+                          //       {schoolCode.id}
+                          // </button>
+                          classInfo?.classInformation?.courseName === "God's Park" || classInfo?.classInformation?.courseName === "God's Park 1" || classInfo?.classInformation?.courseName === "God's Park 2" ||
+                          classInfo?.classInformation?.courseName === "God's Park 3" || classInfo?.classInformation?.courseName === "God's Park 4" ||
+                          classInfo?.classInformation?.courseName === "God's Park 5" || classInfo?.classInformation?.courseName === "God's Park 6" ?
+                          <Button onClick={(e) => goLinkGodsPark(item?.interactive?.path)}  className="m-r-5 color-white tficolorbg-button" size="sm"><i class="fas fa-play" ></i></Button>
                           :
                           <Button onClick={(e) => getInteractiveLink(e, item?.interactive?.path, user?.student?.id, item?.interactive?.id, id)}  className="m-r-5 color-white tficolorbg-button" size="sm"><i class="fas fa-play" ></i></Button>
                         }
