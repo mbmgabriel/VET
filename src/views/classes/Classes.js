e81400ba06a788eaa1bf388fdd6c9fe5b8868d9a
Krent9263 <placia.kent@gmail.com>
2022-08-15 13:36:12 +0800

    add refresh button in class card

--- src/views/classes/Classes.js
+++ src/views/classes/Classes.js
@@ -98,7 +98,7 @@ export default function Classes() {
         <div className='containerpages'>
           {user.isStudent &&
           <>
-          <StudentClassListHeader setJoinClassesModal={setJoinClassesModal} onSearch={onSearch} getPendingClasses={getPendingClasses}  joinClassesToggle={joinClassesToggle} joinClassestModal={joinClassestModal} />
+          <StudentClassListHeader getClassesStudent={getClassesStudent} setJoinClassesModal={setJoinClassesModal} onSearch={onSearch} getPendingClasses={getPendingClasses}  joinClassesToggle={joinClassesToggle} joinClassestModal={joinClassestModal} />
           <CardGroup className='card-group2'>
           {studentClasses.length?
             studentClasses.filter((item) => {
