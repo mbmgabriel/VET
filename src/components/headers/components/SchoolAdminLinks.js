a763d10c226abc8099959a86141a9341f776644d
ferrerUnk <unkferrer@gmail.com>
2022-08-15 10:22:06 +0800

    Display tabs accordinf to subscription type

--- src/components/headers/components/SchoolAdminLinks.js
+++ src/components/headers/components/SchoolAdminLinks.js
@@ -4,16 +4,49 @@ import { UserContext } from '../../../context/UserContext'
 
 export default function SchoolAdminLinks({activeHeader}) {
   const userContext = useContext(UserContext)
-  const { user } = userContext.data
+  const { user } = userContext.data;
+  const subsType = user.subsType;
   
   if(user.isSchoolAdmin){
     return (
       <div className="header-links">
-        <Link className={activeHeader === "dashboard" && 'active'} to="/dashboard">Dashboard</Link>
-        <Link className={activeHeader === "school" && 'active'} to="/school">School</Link>
-        <Link className={activeHeader === "grading" && 'active'} to="/admin/grading">Grading</Link>
-        <Link className={activeHeader === "courses" && 'active'} to="/courses">Courses</Link>
-        <Link className={activeHeader === "classes" && 'active'} to="/schoolAdminClasses">Classes</Link>
+        {
+          subsType.includes('LMS') &&
+          <>
+            <Link className={activeHeader === "dashboard" && 'active'} to="/dashboard">Dashboard</Link>
+            <Link className={activeHeader === "school" && 'active'} to="/school">School</Link>
+            <Link className={activeHeader === "grading" && 'active'} to="/admin/grading">Grading</Link>
+            <Link className={activeHeader === "courses" && 'active'} to="/courses">Courses</Link>
+            <Link className={activeHeader === "classes" && 'active'} to="/schoolAdminClasses">Classes</Link>
+          </>
+        }
+        {
+          subsType === 'Interactives' &&
+          <>
+            <Link className={activeHeader === "dashboard" && 'active'} to="/dashboard">Dashboard</Link>
+            <Link className={activeHeader === "school" && 'active'} to="/school">School</Link>
+            <Link className={activeHeader === "grading" && 'active'} to="/admin/grading">Grading</Link>
+            <Link className={activeHeader === "courses" && 'active'} to="/courses">Courses</Link>
+            <Link className={activeHeader === "classes" && 'active'} to="/schoolAdminClasses">Classes</Link>
+          </>
+        }
+        {
+          subsType === 'Ebooks' &&
+          <>
+            <Link className={activeHeader === "dashboard" && 'active'} to="/dashboard">Dashboard</Link>
+            <Link className={activeHeader === "school" && 'active'} to="/school">School</Link>
+            <Link className={activeHeader === "courses" && 'active'} to="/courses">Courses</Link>
+          </>
+        }
+        {
+          subsType === 'TeacherResources' &&
+          <>
+            <Link className={activeHeader === "dashboard" && 'active'} to="/dashboard">Dashboard</Link>
+            <Link className={activeHeader === "school" && 'active'} to="/school">School</Link>
+            <Link className={activeHeader === "courses" && 'active'} to="/courses">Courses</Link>
+          </>
+        }
+
       </div>
     )
   }
